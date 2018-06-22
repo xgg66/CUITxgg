75a1eaa8050f3c949912953f6882f68f9cfb4dd4
xgg66 <444712055@qq.comgit config --global user.email 444712055@qq.comgit config --global user.email 444712055@qq.com>
2018-06-22 21:05:45 +0800

    增加音乐功能

--- js/new_file.js
+++ js/new_file.js
@@ -70,6 +70,17 @@ $(function() {
 		$(".page4-lightOn").fadeIn();
 		$(".page4-weKnowYou").fadeIn();
 	})
+$(".musicBtn").click(function(){
+	var music=$("#music")[0];
+	if(music.paused){
+		music.paly();
+		$("this").attr("src","img/musicBtn.png")
 
+	}
+	else{
+		music.pause();
+		$("this").attr("src","img/musicBtnOff.png")
+	}
+})
 	
 })
